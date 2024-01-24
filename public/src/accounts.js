a679fea3d2f08fa7a2b7d6b8c0aab4f7d1d6a47e
MuluMehreteab <mulu.mehreteab@gmail.com>
2024-01-24 12:27:53 -0500

    Complete qualified assignment, ...

---
+++ public/src/accounts.js
@@ -0,0 +1,63 @@
+function findAccountById(accounts, id) {
+  return accounts.find(account => account.id === id);
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+}
+
+function sortAccountsByLastName(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+  return accounts.sort((account1, account2) => {
+    const lastName1 = account1.name.last.toUpperCase();
+    const lastName2 = account2.name.last.toUpperCase();
+
+    if (lastName1 < lastName2) {
+        return -1;
+    } else if (lastName1 > lastName2) {
+        return 1;
+    } else {
+        return 0; // Last names are equal
+    }
+});
+}
+
+  
+
+
+function getAccountFullNames(accounts) {
+  // YOUR SOLUTION HERE
+  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+    return accounts.map(account => `${account.name.first} ${account.name.last}`);
+
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
